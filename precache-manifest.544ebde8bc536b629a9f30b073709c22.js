cd6ee8bdcdba67026c76ba1ae0adbe558339df70
Ankush Vangari <ankushvangari@gmail.com>
2018-10-12 00:45:16 -0700

    Updates

--- precache-manifest.544ebde8bc536b629a9f30b073709c22.js
+++ precache-manifest.544ebde8bc536b629a9f30b073709c22.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ankush.vangari.com/static/js/runtime~main.2c45de4f.js"
   },
   {
-    "revision": "949d7cca7151bd1822cb",
-    "url": "/ankush.vangari.com/static/js/main.949d7cca.chunk.js"
+    "revision": "780617293191739a6b8c",
+    "url": "/ankush.vangari.com/static/js/main.78061729.chunk.js"
   },
   {
     "revision": "3bf8d718a66535446819",
     "url": "/ankush.vangari.com/static/js/1.3bf8d718.chunk.js"
   },
   {
-    "revision": "949d7cca7151bd1822cb",
+    "revision": "780617293191739a6b8c",
     "url": "/ankush.vangari.com/static/css/main.8eb08f91.chunk.css"
   },
   {
-    "revision": "a2a4384ab2d8160a6b85629548f34336",
+    "revision": "353aea410a121d7323ff7c03be1a46cb",
     "url": "/ankush.vangari.com/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/ankush.vangari.com/precache-manifest.3b01721ce6d0a09bbf561a4ee14163fb.js"
+  "/ankush.vangari.com/precache-manifest.544ebde8bc536b629a9f30b073709c22.js"
 );
 
 workbox.clientsClaim();
