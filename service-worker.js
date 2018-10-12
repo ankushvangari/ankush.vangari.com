1e9c4836b5246f83069fb95c358a16bbfe298381
Ankush Vangari <ankushvangari@gmail.com>
2018-10-12 13:43:29 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/ankush.vangari.com/precache-manifest.446f21ab14dac47de6d1e22edcc15447.js"
+  "/ankush.vangari.com/precache-manifest.3318581bdd99ebe79dab4e69f62c10b2.js"
 );
 
 workbox.clientsClaim();
