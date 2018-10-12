fdc83b751d953b5015cf113caf693f31a2fdae46
Ankush Vangari <ankushvangari@gmail.com>
2018-10-12 14:00:42 -0700

    Updates

--- precache-manifest.82b45f9fc778584ac829732bd91180ed.js
+++ precache-manifest.82b45f9fc778584ac829732bd91180ed.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ankush.vangari.com/static/js/runtime~main.2c45de4f.js"
   },
   {
-    "revision": "780617293191739a6b8c",
-    "url": "/ankush.vangari.com/static/js/main.78061729.chunk.js"
+    "revision": "e27f82b5f5880776d64d",
+    "url": "/ankush.vangari.com/static/js/main.e27f82b5.chunk.js"
   },
   {
     "revision": "3bf8d718a66535446819",
     "url": "/ankush.vangari.com/static/js/1.3bf8d718.chunk.js"
   },
   {
-    "revision": "780617293191739a6b8c",
+    "revision": "e27f82b5f5880776d64d",
     "url": "/ankush.vangari.com/static/css/main.8eb08f91.chunk.css"
   },
   {
-    "revision": "353aea410a121d7323ff7c03be1a46cb",
+    "revision": "524a574f7bffa3acb1b412ec08e3f19c",
     "url": "/ankush.vangari.com/index.html"
   }
 ];
