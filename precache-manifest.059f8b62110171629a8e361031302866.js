c2b7e9288aff516609b2e5a4a0a20c3e29a6967a
Ankush Vangari <ankushvangari@gmail.com>
2018-10-12 14:16:05 -0700

    Updates

---
+++ precache-manifest.059f8b62110171629a8e361031302866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae0713be70326a243c4e",
+    "url": "/static/js/main.ae0713be.chunk.js"
+  },
+  {
+    "revision": "3bf8d718a66535446819",
+    "url": "/static/js/1.3bf8d718.chunk.js"
+  },
+  {
+    "revision": "ae0713be70326a243c4e",
+    "url": "/static/css/main.8eb08f91.chunk.css"
+  },
+  {
+    "revision": "cf1775106d24435a26a85784e44da163",
+    "url": "/index.html"
+  }
+];
